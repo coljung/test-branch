d359fe7333a9be2fb437215e3242c7a40070abc2
Maxime Gaudreault Proulx <maxime.gaudreault@ssense.com>
2018-08-23 08:33:56 -0400

    Setup of localization framework

--- app/Root.jsx
+++ app/Root.jsx
@@ -1,23 +1,22 @@
-// Dependencies
+import i18n from 'i18next';
 import React, { Component } from 'react';
 import { Router, browserHistory } from 'react-router';
-import { Provider } from 'react-redux';
 import Routes from './Routes';
+import resources from './locales';
 
 // Global Styles
 import './styles/styles';
 
-// Store
-import configureStore from './ConfigureStore';
-
-const store = configureStore();
+i18n.init({
+    lng: 'en',
+    load: 'languageOnly',
+    resources,
+});
 
 export default class Root extends Component {
     render() {
         return (
-            <Provider store={store}>
-                <Router routes={Routes} history={browserHistory} />
-            </Provider>
+            <Router routes={Routes} history={browserHistory} />
         );
     }
 }
