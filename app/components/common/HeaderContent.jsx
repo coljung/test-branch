7035f59001146e399a5eb1d739e07e814a1d697c
Maxime Gaudreault Proulx <maxime.gaudreault@ssense.com>
2018-09-07 10:03:33 -0400

    AppBar following SSENSE styling

--- app/components/common/HeaderContent.jsx
+++ app/components/common/HeaderContent.jsx
@@ -1,12 +1,46 @@
 import i18n from 'i18next';
 import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
-import Logo from './Logo';
+import Grid from '@material-ui/core/Grid';
 
-export default () =>
-    <div className="headerContent">
-        <Logo />
-        <Typography variant="title" color="inherit" noWrap>
-            {i18n.t('appTitle')}
-        </Typography>
-    </div>;
+const styles = theme => ({
+    paper: {
+        padding: theme.spacing.unit * 2,
+        textAlign: 'center',
+        color: theme.palette.text.secondary,
+    },
+    logo: {
+        'vertical-align': 'middle',
+        'margin-top': '-5px',
+        width: '104px',
+        height: 'auto',
+    },
+    title: {
+        display: 'inline-block',
+        'margin-left': '3rem',
+    },
+});
+
+const HeaderContent = props =>
+    <Grid container spacing={24}>
+        <Grid item xs={12}>
+            <div className={props.classes.paper}>
+                <a href="/">
+                    <img src="//res.cloudinary.com/ssenseweb/image/upload/v1471963917/web/ssense_logo_v2.svg" alt="SSENSE"
+                         className={props.classes.logo}>
+                    </img>
+                </a>
+                <Typography variant="display2" color="textPrimary" className={props.classes.title}>
+                    {i18n.t('appTitle')}
+                </Typography>
+            </div>
+        </Grid>
+    </Grid>;
+
+HeaderContent.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(HeaderContent);
--- app/index.jsx
+++ app/index.jsx
@@ -2,7 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
 import * as OfflinePluginRuntime from 'offline-plugin/runtime';
+import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
 import Root from './Root.jsx';
+
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            main: '#ffffff',
+        },
+        secondary: {
+            main: '#212121',
+        },
+    },
+    typography: {
+        fontSize: 12,
+    },
+});
 
 OfflinePluginRuntime.install({
     ServiceWorker: {
@@ -15,7 +30,9 @@ OfflinePluginRuntime.install({
 
 ReactDOM.render(
     <AppContainer>
-        <Root />
+        <MuiThemeProvider theme={theme}>
+            <Root />
+        </MuiThemeProvider>
     </AppContainer>,
     document.getElementById('app'),
 );
