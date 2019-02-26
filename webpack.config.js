4e1add6b898273004647f5bd65d3a2953b9b3efe
coljung <coljung.work@gmail.com>
2019-02-26 13:49:39 -0500

    App updates, package updates, alias added

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,34 @@ const exposedPort = config.get('server.exposedPort');
 const exposedHost = config.get('server.exposedHost');
 
 
+const fs = require('fs');
+
+// absolute paths to all symlinked modules inside `nodeModulesPath`
+// adapted from https://github.com/webpack/webpack/issues/811#issuecomment-405199263
+module.exports = function findLinkedModules(nodeModulesPath) {
+  const modules = []
+
+  fs.readdirSync(nodeModulesPath).forEach(dirname => {
+    const modulePath = path.resolve(nodeModulesPath, dirname)
+    const stat = fs.lstatSync(modulePath)
+
+    if (dirname.startsWith('.')) {
+      // not a module or scope, ignore
+    } else if (dirname.startsWith('@')) {
+      // scoped modules
+      modules.push(...findLinkedModules(modulePath))
+    } else if (stat.isSymbolicLink()) {
+      const realPath = fs.realpathSync(modulePath)
+      const realModulePath = path.resolve(realPath, 'node_modules')
+
+      modules.push(realModulePath)
+    }
+  })
+
+  return modules
+}
+
+
 module.exports = {
     entry: path.join(__dirname, 'src', 'index.jsx'),
     output: {
@@ -17,8 +45,13 @@ module.exports = {
     },
     mode: process.env.NODE_ENV || 'development',
     resolve: {
+        symlinks: false,
         extensions: ['.scss', '.css', '.js', '.jsx', '.json'],
-        modules: [path.resolve(__dirname, 'src'), 'node_modules'],
+        modules: [
+            path.resolve(__dirname, 'src'),
+            'node_modules',
+            // ...findLinkedModules(path.resolve('node_modules')),
+        ],
     },
     devServer: {
         host,
