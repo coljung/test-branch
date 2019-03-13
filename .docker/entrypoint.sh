2815a5c93e11a9f45776de73c690115f21ca6c1d
Nicolas Moreau <nicolas.moreau@ssense.com>
2019-03-13 17:23:34 -0400

    chore: rm submodule

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -3,17 +3,17 @@
 set -e #x  # uncomment for debug output
 
 APP_DIR="/usr/src/app"
-LIB_DIR="/usr/src/app/ui-internal-components-react"
+# LIB_DIR="/usr/src/app/ui-internal-components-react"
 
 npm install -g webpack webpack-cli webpack-dev-server config
 
-cd "${LIB_DIR}"
-npm install
-npm run sass-all
-npm run watch &
-npm link
+# cd "${LIB_DIR}"
+# npm install
+# npm run sass-all
+# npm run watch &
+# npm link
 
 cd "${APP_DIR}"
 npm install
-npm link @ssense/ui-component-library
+# npm link @ssense/ui-component-library
 npm start
