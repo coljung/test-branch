694564213a642222fe0dbc6bf4eaae51b1f18753
Nico Moreau <39535259+nicossense@users.noreply.github.com>
2019-03-27 15:38:16 -0400

    fix: docker-compose entrypoint for component library with workspace (#11)
    * fix: proper npm link in entrypoint for workspace
    
    * chore: update package-lock.json and remove docker-compose file

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -7,13 +7,7 @@ LIB_DIR="/component-library"
 
 npm install -g webpack webpack-cli webpack-dev-server config
 
-cd "${LIB_DIR}"
-npm install
-npm run sass-all
-npm run watch &
-npm link
-
 cd "${APP_DIR}"
 npm install
-npm link @ssense/ui-component-library
+npm link "${LIB_DIR}"
 npm start
