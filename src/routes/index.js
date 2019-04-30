f2deb3f63ebc1d18584172250ba5bff857fa4022
Nicolas Moreau <nicolas.moreau@ssense.com>
2019-04-30 13:59:57 -0400

    missing cnflict fix

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,5 @@
 import HomeIcon from '@material-ui/icons/Home';
 import TableIcon from '@material-ui/icons/TableChart';
-<<<<<<< HEAD
-import GradeIcon from '@material-ui/icons/Grade';
-import ListIcon from '@material-ui/icons/List';
-import Home from '../Home';
-import General from '../exampleViews/GeneralView';
-=======
->>>>>>> feat: PO DEMO WIP
 import NotFound from '../NotFound';
 import { ROUTE_HOME, ROUTE_LISTING } from './constants';
 import ListingView from '../exampleViews/ListingView';
@@ -16,27 +9,7 @@ export const HomeRoute = {
     exact: true,
     name: 'Home',
     icon: HomeIcon,
-<<<<<<< HEAD
-    component: Home,
-};
-
-export const GeneralExampleRoute = {
-    path: ROUTE_EXAMPLES,
-    exact: true,
-    name: 'General',
-    icon: GradeIcon,
-    component: General,
-};
-
-export const TableExampleRoute = {
-    path: ROUTE_TABLES,
-    exact: true,
-    name: 'Tables',
-    icon: TableIcon,
-    component: TableView,
-=======
     component: ListingView,
->>>>>>> feat: PO DEMO WIP
 };
 
 export const NotFoundFallback = {
@@ -47,13 +20,8 @@ export const NotFoundFallback = {
 export const ListingRoute = {
     path: ROUTE_LISTING,
     exact: true,
-<<<<<<< HEAD
-    name: 'Listing View',
-    icon: ListIcon,
-=======
     name: 'Purchase Orders',
     icon: TableIcon,
->>>>>>> feat: PO DEMO WIP
     component: ListingView,
 };
 
